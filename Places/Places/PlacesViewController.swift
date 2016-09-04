64651ae03a5b3ff2af0f16b49e0c48e59edf4680
Deepesh <deepesh259nitk@gmail.com>
2016-09-04 11:31:21 +0100

    Includes
    CoreLocation Logic to get current lat and long
    Changed Initial View controller to have a tableView controller with customised cell.

---
+++ Places/Places/PlacesViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by ITRMG on 2016-04-09.
+//  Copyright © 2016 djrecker. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class PlacesViewController: UITableViewController, CLLocationManagerDelegate {
+    
+    var lat : Double?
+    var long : Double?
+    
+    //for getting current location
+    var locManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Ask for Authorisation from the User.
+        self.locManager.requestAlwaysAuthorization()
+        self.locManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            
+            locManager.delegate = self
+            locManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locManager.startUpdatingLocation()
+            locManager.distanceFilter = 10
+        }
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: Location Methods
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        self.lat = locValue.latitude
+        self.long = locValue.longitude
+    }
+    
+    
+    // MARK: TableView Delegate Methods.
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "cell"
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
+        
+        
+        return cell
+        
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        
+        tableView.estimatedRowHeight = 144.0
+        tableView.rowHeight = UITableViewAutomaticDimension
+        return 155.0
+    }
+
+}
+
---
+++ Places/PlacesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlacesTableViewCell.swift
+//  Places
+//
+//  Created by ITRMG on 2016-04-09.
+//  Copyright © 2016 djrecker. All rights reserved.
+//
+
+import UIKit
+
+class PlacesTableViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+}
